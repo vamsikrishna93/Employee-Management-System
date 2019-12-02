92fe7bafa32f65f11e8908cafa17b1ceec926384
vamsikrishnavarre <vkvarre@gmail.com>
2019-12-02 10:47:36 -0600

    Added update and bulk get to the resource class

--- src/main/java/com/ismav/ems/resource/EmployeeResource.java
+++ src/main/java/com/ismav/ems/resource/EmployeeResource.java
@@ -3,9 +3,12 @@ package com.ismav.ems.resource;
 import com.ismav.ems.model.EmployeeInfo;
 import com.ismav.ems.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/ems")
@@ -24,14 +27,37 @@ public class EmployeeResource {
         return repository.save(employeeInfo);
     }
 
+    @PutMapping("/employee/{id}")
+    private EmployeeInfo updateEmployee(@RequestBody @Valid EmployeeInfo employeeInfo, @PathVariable Long id){
+        EmployeeInfo employeeData = repository.findById(id).get();
+        employeeInfo.setEmployeeId(id);
+        if (!employeeData.equals(employeeInfo)){
+            return repository.save(employeeInfo);
+        }else {
+            System.out.println("Update not required");
+        }
+        return null;
+    }
+
     @DeleteMapping("/employee/{id}")
     private void deleteEmployee(@PathVariable Long id){
         repository.deleteById(id);
     }
 
-    @PatchMapping("/employee/{id}")
-    private void updateEmployee(@PathVariable Long id, @RequestBody EmployeeInfo employeeInfo){
-        repository.save(employeeInfo);
+    @GetMapping("/employee/{id}")
+    private EmployeeInfo retriveEmployee(@PathVariable Long id){
+        return repository.findById(id).get();
+    }
+
+    @GetMapping("/employee")
+    private List<EmployeeInfo> retriveAllEmployees(){
+        Iterable<EmployeeInfo> employeeList = repository.findAll();
+        List<EmployeeInfo> employeeInfos = new ArrayList<>();
+        for (EmployeeInfo info: employeeList
+             ) {
+            employeeInfos.add(info);
+        }
+        return employeeInfos;
     }
 
 }
